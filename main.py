d42785833148762c3b02190628306732e0371c68
marguerite-blondet <marguerite.blondet@edu.esiee.fr>
2025-10-03 08:12:13 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ sys.setrecursionlimit(2000)
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+       selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +18,26 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
-
-    return [ ]
+    n = len(s)
+    if n == 0:
+        return []
+    c = [s[0]]
+    o = [1]
+    if n == 1:
+        return list(zip(c, o))
+    for elt in s[1:]:
+        if elt == c[-1]:
+            o[-1] += 1
+        else:
+            c.append(elt)
+            o.append(1)
+    return list(zip(c, o))
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères passée en argument 
+       selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +45,21 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
 
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
-
-    return []
-    
+    if len(s) == 0:
+        return []
+    i = 1
+    while i < len(s) and s[i] == s[0]:
+        i += 1
+    return [(s[0], i)] + artcode_r(s[i:])
 
 #### Fonction principale
-
-
 def main():
+    '''fonction principale, test des fonctions secondaires'''
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
